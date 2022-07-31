5639b1cec122ce03573528cf28857b84f8227c1e
pascal111-fra <82375522+pascal111-fra@users.noreply.github.com>
2022-07-31 02:55:29 +0200

    Add files via upload

--- proj04.d
+++ proj04.d
@@ -4,13 +4,16 @@ import std.stdio;
 import dcollect;
 import std.string;
 import std.conv;
+import std.algorithm;
 
 
 int main(string[] args)
 {
 
     string sentence_x,
-    sent_result, token2;
+    sent_result, temp;
+    const string[] fixed=["is", "has", "was"];
+    const string[] delim=[".", "?","'",";","\"",":",";","!",","];
     string[] sentence_tokens;
 
     writeln(strstring(5,"*")," No tail \"s\" ",strstring(5,"*"));
@@ -19,13 +22,28 @@ int main(string[] args)
     write("Compose a line: ");
     sentence_x=strip(readln());
 
-    sentence_tokens=sentence_x.d_strtok(" ,;.:!?\"'");
+    sentence_tokens=sentence_x.d_strtok(" ");
 
     foreach(token; sentence_tokens){
-        token2=token.strrtrim;
-        if(token2[token2.length-1]=='s')
-            token2=strdel(token2, token2.length-1,1);
-            sent_result~=token2~" ";}
+
+        token=token.strrtrim;
+        temp="";
+
+        if(token.canFind("s")){
+
+
+            while(delim.canFind(to!string(token[token.length-1]))){
+                temp~=token[token.length-1];
+                token=token.strdel(token.length-1,1);}
+
+            if(token[token.length-1]=='s' &&
+            !fixed.canFind(token))
+                token=token.strdel(token.length-1,1);}
+
+
+            sent_result~=token~temp~" ";}
+
+    //sent_result~=".";
 
     sent_result.writeln;
 
