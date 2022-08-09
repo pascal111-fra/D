b698fa00e00b0c1e32f05482a85c293718fc4786
pascal111-fra <82375522+pascal111-fra@users.noreply.github.com>
2022-08-09 02:32:47 +0200

    Add files via upload

---
+++ proj07.d
@@ -0,0 +1,55 @@
+module proj07;
+
+import std.stdio;
+import std.algorithm;
+import std.range;
+import core.stdc.stdlib;
+
+int main()
+{
+
+    double x, y;
+    int choice;
+
+new_again:
+    
+    x=0;
+    y=0;
+
+    write("Enter a number: ");
+    readf(" %s\n", &x);
+    y=x;
+    writeln;
+
+choice_again:
+
+    writeln("1) addition 2) subtraction 3) 
+    multiplication 4) division 0) exit -1) new");
+    ": ".write;
+
+    readf(" %s\n", &choice);
+
+    if(!only(1,2,3,4,0,-1).canFind(choice))
+        goto choice_again;
+    else if(choice==0)
+        exit(0);
+    else if(choice==-1)
+        goto new_again;
+
+    write("Enter a number: ");
+    readf(" %s\n", &x);
+
+    switch(choice){
+
+        case 1: y+=x; break;
+        case 2: y-=x; break;
+        case 3: y*=x; break;
+        case 4: y/=x; break;
+        default: break;}
+
+    y.writeln;
+
+    goto choice_again;
+
+    return 0;
+}
